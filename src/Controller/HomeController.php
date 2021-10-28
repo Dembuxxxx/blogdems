87ffeab759e6bebd4965e4f2cfb032681a7d8b25
Dembuxxxx <dems75018@hotmail.fr>
2021-10-28 18:49:14 +0200

    add lign use entitypost in homeController

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+
+use App\Entity\Post;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -12,7 +14,8 @@ class HomeController extends AbstractController
      * @Route("/home", name="home")
      */
     public function index(): Response
-    {
+    {   
+
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
         ]);

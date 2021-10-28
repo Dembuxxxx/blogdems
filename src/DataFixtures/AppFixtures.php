fd5b31c3d9a192474d1132eabe99aa5c837f3fbe
Dembuxxxx <dems75018@hotmail.fr>
2021-10-28 20:44:56 +0200

    ajout post dans fixtures avec boucle pour dix articles

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -12,6 +12,16 @@ class AppFixtures extends Fixture
         // $product = new Product();
         // $manager->persist($product);
 
+        for($i = 0; $i < 10; $i++) {
+            $post = new Post();
+            $post->setTitle('Titre 1')
+                ->setContent("Lorem ipsum")
+                ->setAuthor('Demba Soumare')
+                ->setCreateAt(new \DateTime());
+            
+            $manager->persist($post);
+        }
+
         $manager->flush();
     }
 }

0d6a3c1e02aadd07664e79e4d0a31f8c79507dc4
Dembuxxxx <dems75018@hotmail.fr>
2021-10-29 01:12:25 +0200

    add road post

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,8 +2,7 @@
 
 namespace App\Controller;
 
-
-
+use App\Repository\PostRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -18,17 +17,16 @@ class HomeController extends AbstractController
     /**
      * @Route("/home", name="home")
      */
-    public function index(): Response
+    public function index(PostRepository $ripo)
     {   
 
-        
+      $posts = $ripo->findAll();  
 
           
 
 
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
-            'post' => 'post',
+            'posts' => $posts
             
            
         ]);
